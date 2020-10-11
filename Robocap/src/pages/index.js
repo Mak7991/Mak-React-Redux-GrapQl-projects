2d644fbee480d7f57b080f7c160c60adc82b7de3
Mak7991 <alikhanmaaz1997@gmail.com>
2020-10-11 15:21:35 +0500

    react-redux-graphql-work

--- Robocap/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Login from "pages/Login/Login";
-import GoalPanel from "pages/GoalPanelHome/GoalPanel"
-import Performance from "pages/Performance/Performance"
-import NotFound from "pages/NotFound/NotFound"
-
-export{
-    Login, GoalPanel, Performance, NotFound
-}
--- Robocap/src/redux/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import ReduxThunk from "redux-thunk";
-import rootReducer from "redux/reducers";
-
-export const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
